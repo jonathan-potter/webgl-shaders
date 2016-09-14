f8b9348695315bb5380bf4d69c995a571ad63ee8
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-09-14 15:31:09 -0600

    moves geometry setup back to configureWebGL
    - probably should rename a few things

--- javascript/WebGL/configureWebGL.js
+++ javascript/WebGL/configureWebGL.js
@@ -1,6 +1,9 @@
 /* utility */
 import compileShader from 'webgl-utilities/compileShader'
 import createProgram from 'webgl-utilities/createProgram'
+import prepareGeometry from 'webgl-utilities/prepareGeometry'
+
+const { Float32Array } = window
 
 export default function ({ context, fragmentShaderSource, vertexShaderSource }) {
   const vertexShader = compileShader({
@@ -20,6 +23,19 @@ export default function ({ context, fragmentShaderSource, vertexShaderSource }) 
     fragmentShader: fragmentShader,
     context
   })
+
+  context.linkProgram(program)
+
+  /* eslint-disable no-multi-spaces, indent */
+  const vertices = new Float32Array([
+    -1.0,  1.0, // top left
+    -1.0, -1.0, // bottom left
+     1.0,  1.0, // top right
+     1.0, -1.0  // bottom right
+  ])
+  /* eslint-enable no-multi-spaces, indent */
+
+  prepareGeometry({ context, program: program, vertices })
 
   return program
 }
--- javascript/WebGL/initializeWebGL.js
+++ javascript/WebGL/initializeWebGL.js
@@ -1,14 +1,13 @@
 import { getCurrentFractal } from 'reducers'
 import configureWebGL from 'webgl/configureWebGL'
 import drawFrame from 'webgl/drawFrame'
-import prepareGeometry from 'webgl-utilities/prepareGeometry'
 
 /* shaders */
 import vertexShaderSource from 'shaders/vertexShader.glsl'
 import juliaShaderSource from 'shaders/julia.glsl'
 import collatzShaderSource from 'shaders/collatz.glsl'
 
-const { requestAnimationFrame, Float32Array } = window
+const { requestAnimationFrame } = window
 
 export default ({ store }) => {
   const canvas = document.getElementById('main')
@@ -40,20 +39,7 @@ export default ({ store }) => {
       currentFractal = getCurrentFractal(state)
       const currentProgram = programs[currentFractal]
 
-      context.linkProgram(currentProgram)
       context.useProgram(currentProgram)
-
-      /* eslint-disable no-multi-spaces, indent */
-      const vertices = new Float32Array([
-        -1.0,  1.0, // top left
-        -1.0, -1.0, // bottom left
-         1.0,  1.0, // top right
-         1.0, -1.0  // bottom right
-      ])
-      /* eslint-enable no-multi-spaces, indent */
-
-      prepareGeometry({ context, program: currentProgram, vertices })
-
       requestAnimationFrame(drawFrame.bind(null, {
         canvas,
         context,
